8cbeb93cbd9e852ac146c00e8ae9777d86fd1f60
Vladtsrez <Competitionvladinf@gmail.com>
2024-04-24 23:03:18 +0300

    Update title, imports, add normalize, local font, font-face

--- src/main.js
+++ src/main.js
@@ -4,3 +4,10 @@ import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper';
 // import Swiper styles
 import 'swiper/css';
+
+import './js/header.js';
+import './js/about-me.js';
+import './js/benefits.js';
+import './js/projects.js';
+import './js/reviews.js';
+import './js/footer.js';
