7dbba2384fe1746d78f069bc0fb4f83e91f72576
Yaroslaw <xxjokerxx933@gmail.com>
2024-04-26 18:31:06 +0300

    add styles, render-function, swiper-btn

---
+++ src/js/reviews.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+   modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+    direction: 'horizontal',
+});
+console.log(swiper);
+
+const review = document.querySelector('.reviews-list'); 
+
+
+async function fetchReviews() {
+    const url = 'https://portfolio-js.b.goit.study/api/reviews';
+    try{
+        const response = await axios(url);
+        return response.data;
+    }
+    catch (error) {
+        throw new Error('Failed to fetch reviews');
+    };
+}
+
+async function renderReviews() {
+    const data = await fetchReviews();
+    console.log(data);
+    review.innerHTML = '';
+    const reviewElements = data.map(reviews => {
+        return `
+        <li class='reviews-item swiper-slide' id='${reviews._id}'>
+  <img class='reviews-photo' src="${reviews.avatar_url}" alt="author_photo">
+  <h3 class='author-name'>${reviews.author}</h3>
+  <p class='author-review'>${reviews.review}</p>
+</li>`
+    }).join('');
+
+    review.insertAdjacentHTML('beforeend', reviewElements);
+}
+
+renderReviews();
+
