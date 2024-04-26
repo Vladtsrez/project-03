537c21a6f906e769046bac3b364ded96b3ede5d3
Vitaly112 <149366473+Vitaly112@users.noreply.github.com>
2024-04-26 20:09:23 +0300

    header html/css

---
+++ src/js/header.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  const menuOpen = document.querySelector(".header-menu-open");
+  const menuList = document.querySelector(".header-menu-list");
+  const menuLinks = document.querySelectorAll(".header-menu-link");
+
+  menuOpen.addEventListener("click", function() {
+    menuList.style.display = (menuList.style.display === "flex") ? "none" : "flex";
+  });
+
+  menuLinks.forEach(function(link) {
+    link.addEventListener("click", function() {
+      menuList.style.display = "none"; 
+    });
+  });
+});
