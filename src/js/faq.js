81ff3b7032421c86164f69cd88acbb8f9b32086a
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-04-26 23:51:33 +0200

    add js and updated html

---
+++ src/js/faq.js
@@ -0,0 +1,18 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const container = document.querySelector('.accordion-container');
+const accordion = new Accordion(container, {
+  duration: 600,
+  showMultiple: true,
+  openOnInit: [],
+  elementClass: 'ac',
+  triggerClass: 'ac-trigger',
+  panelClass: 'ac-panel',
+  activeClass: 'is-active',
+  ariaEnabled: true,
+  collapse: true,
+  onlyChildNodes: true,
+  openOnInit: [],
+  onOpen: currEl => console.log('Open!', currEl),
+});
