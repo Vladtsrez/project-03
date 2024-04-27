a67639f6c5b9c80beb10cc972e8af5b963cf4351
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-04-27 03:13:05 +0200

    add some css styles, but i didn't find the way to change styles from library accordion

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,5 +1,5 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import 'accordion-js/dist/accordion.min.css';
 
 const container = document.querySelector('.accordion-container');
 const accordion = new Accordion(container, {
